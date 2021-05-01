5f950553008c701f1ac61161068054b4ee1af4ab
Ali Adruich <adruich.ali@gmail.com>
2021-05-01 20:20:45 +0000

    initial commit

---
+++ components/Nav.js
@@ -0,0 +1,20 @@
+import requests from "../utils/requests"
+import {useRouter} from "next/router";
+
+function Nav() {
+    const router = useRouter();
+    return (
+ <nav>
+            <div className="flex px-8 sm:px-15 text-2xm whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide ">
+{Object.entries(requests).map(([key, {title, url}]) => (
+    <h2 
+    onClick={()=> router.push(`/?genre=${key}`)}
+    key={key} 
+    className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500">{title}</h2>
+))}
+ </div>
+ <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"/>
+        </nav> 
+    )
+}
+export default Nav
